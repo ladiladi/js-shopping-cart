8b1cc943f08593f3d806368716fda7bd47d3d5c6
ladiladi <38681465+ladiladi@users.noreply.github.com>
2020-07-28 17:53:07 +0300

    Add cart page items
    Ion icons, buttons and cart page stylings need tweaking next.

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ let products = [
     },
     {
         name: 'Polo shirt',
-        tag: 'poloshirt',
+        tag: 'polotshirt',
         price: 16,
         inCart: 0
     },
@@ -100,4 +100,46 @@ function totalCost(product) {
     }
 
 }
+
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem("totalCost");
+
+    if( cartItems && productContainer ) {
+        productContainer.innerHTML = "";
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <ion-icon name="close-circle"></ion-icon>
+                <img src="./images/${item.tag}.png"
+                <span>${item.name}</span>
+            </div>
+            <div class="price">${item.price}</div>
+            <div class="quantity">
+                <ion-icon name="arrow-down-circle-outline"></ion-icon>
+                <span>${item.inCart}</span>
+                <ion-icon name="arrow-up-circle-outline"></ion-icon>
+            </div>
+            <div class="total">
+                ${item.inCart * item.price},00€
+            </div>
+            `;
+        });
+
+        productContainer.innerHTML += `
+            <div class="basketTotalContainer">
+                <h4 class="basketTotalTitle">
+                    Basket Total
+                </h4>
+                <h4 class="basketTotal">
+                    ${cartCost},00€
+                </h4>
+            </div>
+        `;
+    }
+}
+
 onLoadCartNumbers();
+displayCart();
